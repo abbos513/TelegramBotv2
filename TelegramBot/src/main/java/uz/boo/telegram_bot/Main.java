3299557d72968f9480feb187c44f601ff5efd822
abbos <abbos513@gmail.com>
2022-02-15 21:58:05 +0500

    spring boot app

--- TelegramBot/src/main/java/uz/boo/telegram_bot/Main.java
+++ TelegramBot/src/main/java/uz/boo/telegram_bot/Main.java
@@ -3,11 +3,15 @@ package uz.boo.telegram_bot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
 	public static void main(String[] args) {
 		// Initialize Api Context
 		ApiContextInitializer.init();
+		SpringApplication.run(Main.class, args);
 
 		// Instantiate Telegram Bots API
 		TelegramBotsApi botsApi = new TelegramBotsApi();
